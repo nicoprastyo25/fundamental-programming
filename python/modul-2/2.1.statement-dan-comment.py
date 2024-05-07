248ab0a414270b313c1ad7c29175cd1570a12b83
nicoprastyo25 <nicoprastyo93@gmail.com>
2024-05-07 10:50:40 +0700

    python-add-2.1 statement-dan-comment.py

---
+++ python/modul-2/2.1.statement-dan-comment.py
@@ -0,0 +1,36 @@
+# Konsep Statement
+
+# Apa itu statement?
+# statement adalah instruksi-instruksi yang kita tulis pada perangkat editor (Visual studio code) yang kemudian mesin (Python interpreter) akan memahami dan mengeksekusi sekumpulan instruksi tersebut.
+
+# misal.
+# contoh 1:
+print("Ini adalah instruksi pertama saya")
+
+# contoh 2:
+for i in range(1, 11):
+    print(i)
+    if i == 5:
+        break
+    if True: print('Hello'); a = 5
+
+# Konsep Comment
+
+# Apa itu Comment?
+# Comment adalah catatan-catatan yang kita berikan saat melakukan penulisan bahasa pemrograman yang bertujuan untuk memberikan daskripsi terhadap apa yang sedang dilakukan
+
+# misal.
+
+# # ini adalah instruksi yang memuat 1 kalimat
+# variabel_saya = "Nilai-nilai yang disimpan di variabel"
+
+
+# Praktik
+batas_maksimal = 1
+
+for b in range(1, batas_maksimal):
+    print(b)
+    if b == 5:
+        break
+
+if True: print('hallo'); c = 5
---
+++ python/modul-2/2.2.variabel-dan-data-type.py
@@ -0,0 +1,28 @@
+# Konsep Variable
+
+# Apa itu Variable?
+# Variable adalah lokasi memori yang dicadangkan untuk menyimpan nilai-nilai di suatu pemrograman
+
+# Contoh:
+# nama variabel = value/nilai
+# variabel_saya = "Nilai-nilai yang disimpan di variabel"
+
+# Aturan penulisan variable python
+# - Karakter pertama harus berupa huruf atau garis bawah/underscore _.
+# - Karakter selanjutnya dapat berupa huruf, garis bawah/underscore _ atau angka.
+# - karakter pada nama variable bersifat sensitif (case-sensitif) Artinya huruf kecil dan huruf besar dibedakan. sebagai contoh, variabel namaDepan dan namadepan adalahvariabel
+
+# Aturan penulisan variable python contoh:
+# variable_saya (BENAR)
+# variable123 (BENAR)
+# VariabelSaya (BENAR)
+# @variable (SALAH)
+
+# Contoh penggunaan variable dalam pemrograman python
+nama = "Nico prastyo"   #nilai awal
+print(nama)             #mencetak nilai nama
+
+# value(nico prastyo) disimpan pada variabel (nama) lalu di print/tampilkan menggunakan variable nama
+
+
+# Konsep Data Type
